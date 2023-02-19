4720ebc286164c1107c1218f2a671a7c81363995
Carlos-Gabriel-Rojas <carlosgabrielrojas.dev@gmail.com>
2023-02-19 12:16:28 -0300

    addTask function inserting task inside the taskList ul

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const date= document.querySelector("#date")
 const list= document.querySelector("#taskList")
 const input= document.querySelector("#addTaskInput")
 const enterBtn= document.querySelector("#enterBtn")
+
+//add task function
+function addTask(task) {
+    const element= `<i class="far fa-cicle co" data="done" id="0"></i>
+                    <p class="text">${task}</p>
+                    <i class="fas fa-trash de" data="deleted" id="0"></i>
+                   `
+    list.insertAdjacentHTML("beforeend",element)
+}
