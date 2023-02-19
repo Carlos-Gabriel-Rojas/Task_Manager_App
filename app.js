4c2e68dbffb7343e15fe5c765013ab3b65747053
Carlos-Gabriel-Rojas <carlosgabrielrojas.dev@gmail.com>
2023-02-19 12:05:17 -0300

    declared the variables

---
+++ app.js
@@ -0,0 +1,4 @@
+const date= document.querySelector("#date")
+const list= document.querySelector("#taskList")
+const input= document.querySelector("#addTaskInput")
+const enterBtn= document.querySelector("#enterBtn")
