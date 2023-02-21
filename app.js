224bf0ecd964b5998f799a0d753a813c5aaf4f06
Carlos-Gabriel-Rojas <carlosgabrielrojas.dev@gmail.com>
2023-02-21 12:26:22 -0300

    enterBtn event listener and function to add te text you put on the input

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const enterBtn= document.querySelector("#enterBtn")
 
 //add task function
 function addTask(task) {
-    const element= `<i class="far fa-cicle co" data="done" id="0"></i>
+    const element= `<li>
+                    <i class="far fa-cicle co" data="done" id="0"></i>
                     <p class="text">${task}</p>
                     <i class="fas fa-trash de" data="deleted" id="0"></i>
+                    </li>
                    `
     list.insertAdjacentHTML("beforeend",element)
 }
+
+enterBtn.addEventListener("click", ()=> {
+    const task= input.value
+    if (task) {
+       addTask(task) 
+    }
+    input.value=""
+})
